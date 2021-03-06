aca7398a5aaec4bcde0982dd7ee42b76b7f444e0
Laurent Bouriez <laurent@bouriez.org>
2021-03-05 22:24:16 -0500

    Improve for zip file

--- GenerateReport/Helper/IO.cs
+++ GenerateReport/Helper/IO.cs
@@ -2,6 +2,7 @@
 using Helper.Model;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 
 namespace GenerateReport.Helper
@@ -28,12 +29,22 @@ namespace GenerateReport.Helper
         public static IEnumerable<DirectoryInfo> GetDirectory(string path = "")
         {
             var dirs = Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly);
+            // We may be in the case of zip file, trying...
+            if (dirs.Length == 0)
+            {
+                var zipFiles = GetFiles(path, "zip");
+                zipFiles.ToList().ForEach(x => {
+                    ZipFile.ExtractToDirectory(x.FullName, path);
+                    //x.Delete();
+                });
+                dirs = Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly);
+            }
             return dirs.Select(x => new DirectoryInfo(x));
         }
 
-        public static IEnumerable<FileInfo> GetFiles(string path = "")
+        public static IEnumerable<FileInfo> GetFiles(string path = "", string ext = "pdf")
         {
-            var files = Directory.GetFiles(path, "*.pdf*", SearchOption.TopDirectoryOnly);
+            var files = Directory.GetFiles(path, $"*.{ext}*", SearchOption.AllDirectories);
             return files.Select(x => new FileInfo(x));
         }
     }
